4ccc14d9319e09e56b36dd76d2f4ef9329b838c3
semevi <140612005+semevi@users.noreply.github.com>
2026-01-09 13:22:03 +0000

    Update vite.config.ts

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',                        // для Vercel
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000' // локально проксируем на бэк
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
